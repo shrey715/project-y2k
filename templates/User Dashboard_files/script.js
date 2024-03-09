0604b77f1e9410bcd3415e09de4e9fb94ba591ef
kinomorph <shreyasdeb@gmail.com>
2024-03-10 00:32:47 +0530

    search functionality added

---
+++ templates/User Dashboard_files/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    var navtexts = document.getElementsByClassName("nav-text");
+    var navbutton = document.getElementById("sidebar-btn");
+
+    for (var i = 0; i < navtexts.length; i++) {
+        navtexts[i].classList.toggle("show-nav-text");
+    }
+
+    navbutton.classList.toggle('bxs-chevrons-left');
+    navbutton.classList.toggle('bxs-chevrons-right');
+}
+
+function toggleProfile(){
+    var profile = document.getElementById("profileDropdown");
+    profile.classList.toggle("show-profile");
+}
--- video_creator.py
+++ video_creator.py
@@ -1,1 +1,43 @@
-from moviepy.editor import ImageClip, concatenate_videoclips
+from moviepy.editor import ImageClip, concatenate_videoclips, CompositeVideoClip
+from moviepy.video.fx.all import loop
+import pymysql
+import io
+from PIL import Image
+import numpy as np
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                            user='root',
+                            password='Noshu@1211',
+                            db='y2k_database',
+                            cursorclass=pymysql.cursors.DictCursor)
+
+images = []
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM images')
+for row in cursor:
+    image = Image.open(io.BytesIO(row['image']))
+    width, height = image.size
+    new_height = 720
+    new_width = int(new_height * width / height)
+    image = image.resize((new_width, new_height))
+    images.append(np.array(image, dtype=np.uint8))  # Convert to integer type
+cursor.close()
+
+# Ask the user for the duration of the transition effect
+transition_duration = float(input("Enter the duration of the transition effect: "))
+
+clips = []
+for i, m in enumerate(images):
+    clip = ImageClip(m).set_duration(5).fadein(transition_duration).fadeout(transition_duration)
+    if i % 2 == 0:  # if the index is even
+        # Create a spinning effect
+        spinning_clip = CompositeVideoClip([clip.rotate(i) for i in range(360)])
+        clip = loop(spinning_clip, duration=5)
+    clips.append(clip)
+
+final_video = concatenate_videoclips(clips, method="compose")
+
+final_video.write_videofile("my_concatenation.mp4", fps=24)
+
+connection.close()
